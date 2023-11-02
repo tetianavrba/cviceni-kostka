0037e93be40192196e08518a086d13d614bc4d9c
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-11-02 10:53:21 +0100

    JS

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+let viditelneCislo = 1;
+const kostka = document.querySelector('.dice');
+const pootocKostkou = () => {
+  viditelneCislo = viditelneCislo + 1;
+  if (viditelneCislo === 7) {
+    viditelneCislo = 1;
+  }
+  kostka.src = `img/side${viditelneCislo}.svg`;
+};
+document.addEventListener('keydown', pootocKostkou);
